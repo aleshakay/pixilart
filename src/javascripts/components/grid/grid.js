463af1f7d67c42d42deb30e957f54cdf65846a98
alesha reed <aleshak83@gmail.com>
2019-10-07 19:27:56 -0500

    create grid

---
+++ src/javascripts/components/grid/grid.js
@@ -0,0 +1,18 @@
+import './grid.scss';
+import utilities from '../../helpers/utilities';
+
+const makeGrid = () => {
+  let domString = '<table>';
+  for (let r = 0; r < 25; r += 1) {
+    domString += '<tr>';
+    for (let c = 0; c < 25; c += 1) {
+      domString += '<td></td>';
+    }
+    domString += '</tr>';
+  }
+  domString += '</table>';
+  utilities.printToDom('grid', domString);
+};
+
+
+export default { makeGrid };
